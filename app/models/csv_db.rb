5528a2b23db1e266f1aa31cd0d169402d39a7c32
Pau Perez <saulopefa@gmail.com>
2018-01-30 08:53:19 +0100

    Extract Struct from CSV row hash
    This gives names to the rows and thus improves a lot the readability of
    the code. Better names than hash indexes.

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -1,23 +1,34 @@
 require "csv"
 
 class CsvDb
+  Row = Struct.new(
+    :username,
+    :member_id,
+    :entry_date,
+    :date_of_birth,
+    :email,
+    :phone,
+    :alt_phone,
+    :gender
+  )
+
   class << self
     def user_from_row(row)
       User.new(
-        username: row[2..4].join(" "),
-        date_of_birth: row[6],
-        email: row[9],
-        phone: row[7],
-        alt_phone: row[8],
-        gender: User::GENDERS[row[5].to_i - 1]
+        username: row.username,
+        date_of_birth: row.date_of_birth,
+        email: row.email,
+        phone: row.phone,
+        alt_phone: row.alt_phone,
+        gender: row.gender
       )
     end
 
     def member_from_row(row, user, organization, errors)
-      member = organization.members.create(member_uid: row[0],
-                                           entry_date: row[1],
+      member = organization.members.create(member_uid: row.member_id,
+                                           entry_date: row.entry_date,
                                            user_id: user.id)
-      errors.push(member_id: row[0], email: row[9],
+      errors.push(member_id: row.member_id, email: row.email,
                   errors: member.errors.full_messages
                  ) if member.errors.present?
     end
@@ -29,20 +40,30 @@ class CsvDb
       if user.save
         member_from_row(row, user, organization, errors)
       else
-        errors.push(member_id: row[0], email: row[9],
+        errors.push(member_id: row.member_id, email: row.email,
                     errors: user.errors.full_messages)
       end
     end
 
     def convert_save(organization_id, csv_data)
       data = csv_data.read
-      # data.force_encoding Encoding::ISO_8859_1
-      # normalized_data = data.encode(Encoding::UTF_8)[1..-1]
       organization = Organization.find(organization_id)
       errors = []
-      CSV.parse(data, headers: false) do |row|
+
+      CSV.parse(data, headers: false) do |data_row|
+        row = Row.new(
+          data_row[2..4].join(" "),
+          data_row[0],
+          data_row[1],
+          data_row[6],
+          data_row[9],
+          data_row[7],
+          data_row[8],
+          User::GENDERS[data_row[5].to_i - 1]
+        )
         process_row(row, organization, errors)
       end
+
       organization.ensure_reg_number_seq!
       errors
     end
